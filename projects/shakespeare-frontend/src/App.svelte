2812a1c16368b76ee26f2a143d1bc3ef0225807c
Sam Bulatov <mephistorine@gmail.com>
2021-06-13 23:03:15 +0300

    feat: Work on app

--- projects/shakespeare-frontend/src/App.svelte
+++ projects/shakespeare-frontend/src/App.svelte
@@ -1,35 +1,170 @@
 <script lang="ts">
-  export let name: string
+  import Icon from './shared/components/Icon.svelte'
 
   let windowMode: 'login' | 'register' | null = null
 
+  interface Book {
+    starCount: number
+    posterUrl: string
+    viewCount: number
+    title: string
+  }
+
+  const books: Book[] = [
+    {
+      posterUrl: 'https://images-na.ssl-images-amazon.com/images/I/51c-tuESgdL._SX379_BO1,204,203,200_.jpg',
+      title: 'Programming Rust',
+      starCount: 4.5,
+      viewCount: 1100
+    },
+    {
+      posterUrl: 'https://cdn1.ozone.ru/s3/multimedia-2/6012014246.jpg',
+      title: 'Повелитель мух',
+      starCount: 5,
+      viewCount: 1100
+    },
+    {
+      posterUrl: 'https://images-na.ssl-images-amazon.com/images/I/51c-tuESgdL._SX379_BO1,204,203,200_.jpg',
+      title: 'Programming Rust',
+      starCount: 4.5,
+      viewCount: 1100
+    },
+    {
+      posterUrl: 'https://images-na.ssl-images-amazon.com/images/I/51c-tuESgdL._SX379_BO1,204,203,200_.jpg',
+      title: 'Programming Rust',
+      starCount: 4.5,
+      viewCount: 1100
+    }
+  ]
+
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>{windowMode}</p>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-  <a on:click={() => { windowMode = 'login' }}>login</a>
+<svelte:head>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+    }
+
+    img {
+      max-width: 100%;
+      height: auto;
+    }
+  </style>
+</svelte:head>
+
+<main class="main">
+  <section class="recommended-books-container">
+    <h1 class="main-title">Что хотите почитать?</h1>
+    <div class="recommended-books">
+      <div class="filter">
+        <ul class="filter-list">
+          <li>Популярные</li>
+          <li>Все книги</li>
+        </ul>
+      </div>
+
+      <div class="books-container">
+        <ul class="book-list">
+          {#each books as book}
+            <li>
+              <div class="poster">
+                <img src={book.posterUrl}>
+              </div>
+              <div class="book-info">
+                <h3 class="book-info-title">{book.title}</h3>
+                <div class="rating-container">
+                  <Icon name="star" size="11px"/>
+                  <span>{book.starCount}</span>
+                </div>
+                <div class="view-count">
+                  <Icon name="eye" size="11px"/>
+                  <span>{book.viewCount}</span>
+                </div>
+              </div>
+            </li>
+          {/each}
+        </ul>
+      </div>
+    </div>
+  </section>
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+  .main-title {
+    font-size: 3em;
+    margin-top: 0;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  .recommended-books-container {
+    padding: 1rem;
+    background-color: #4b28e6;
+    color: #fff;
+    margin-bottom: 6rem;
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  .filter {
+    margin-bottom: 1rem;
+  }
+
+  .filter-list,
+  .book-list {
+    margin: 0;
+    padding: 0;
+  }
+
+  .filter-list {
+    list-style: none;
+    display: flex;
+  }
+
+  .filter-list li:not(:last-child) {
+    margin-right: 1rem;
+  }
+
+  .books-container {
+    overflow-x: auto;
+    padding-bottom: 1rem;
+    margin-right: -1rem;
+  }
+
+  .book-list {
+    display: flex;
+    list-style: none;
+  }
+
+  .book-list li {
+    display: flex;
+    background-color: #fff;
+    color: #000;
+    padding: 0.5rem;
+    border-radius: 15px;
+  }
+
+  .book-list li:not(:last-child) {
+    margin-right: 1rem;
+  }
+
+  .book-list li .poster {
+    width: 100px;
+    margin-right: 1rem;
+  }
+
+  .poster img {
+    border-radius: 15px;
+  }
+
+  .book-info-title {
+    margin-top: 0;
+  }
+
+  .rating-container,
+  .view-count {
+    display: flex;
+    align-items: center;
+  }
+
+  .rating-container .icon-container {
+    width: 11px;
   }
 </style>
---
+++ projects/shakespeare-frontend/src/shared/components/Icon.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { ICONS } from '../icons'
+
+  export let name: keyof typeof ICONS
+
+  export let size: string = 'auto'
+
+</script>
+
+<div class="icon-container" style="--icon-size: {size}">
+  {#if ICONS[ name ]}
+    {@html ICONS[ name ]}
+  {/if}
+</div>
+
+<style>
+  .icon-container {
+    width: var(--icon-size);
+    height: var(--icon-size);
+  }
+
+  .icon-container :global(svg) {
+    display: block;
+  }
+</style>
